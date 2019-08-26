937e37f266ae9384fb430167813fb989435e0c17
Allie <allison.tpttn@gmail.com>
2019-08-26 13:26:26 -0500

    working delete button for animal and employees

--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -21,13 +21,30 @@ class EmployeeList extends Component {
             })
     }
 
+    deleteEmployee = id => {
+        EmployeeManager.delete(id)
+            .then(() => {
+                EmployeeManager.getAll()
+                    .then((newEmployees) => {
+                        this.setState({
+                            employees: newEmployees
+                        })
+                    })
+            })
+    }
+
     render() {
         console.log("Employee LIST: Render");
 
         return (
             <div className="container-cards">
-                {this.state.employees.map(employee => 
-                <EmployeeCard key={employee.id} employee={employee} />)}
+                {this.state.employees.map(employee =>
+                    <EmployeeCard
+                        key={employee.id}
+                        employee={employee}
+                        deleteEmployee={this.deleteEmployee}
+                    />
+                )}
             </div>
         )
     }
